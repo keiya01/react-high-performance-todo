228e1c390acd2640eeba234de0d075ed385a2fe2
keiya01 <keiya.s.0210@gmail.com>
2020-02-14 17:44:56 +0900

    Fix structure

--- src/components/page/Top.tsx
+++ src/components/page/Top.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+
+const Container = styled.div`
+  background-color: #292929;
+  width: 100%;
+  height: 100vh;
+`;
 
 const Top: React.FC = () => {
   return (
-    <div>
+    <Container>
       <h1>Top Page!</h1>
       <Link to="/about">about</Link>
-    </div>
+    </Container>
   )
-}
+};
 
 export default Top;
