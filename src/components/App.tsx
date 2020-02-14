2ff44581f1a3eb31e46b8a7032b4481fe279eb73
keiya01 <keiya.s.0210@gmail.com>
2020-02-14 17:45:23 +0900

    Fix index.tsx

---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Router from "src/components/router";
+
+const App: React.FC = () => {
+  return (
+    <Router/>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Router from "src/components/Router";
+import App from "src/components/App";
 
-ReactDOM.render(<Router/>, document.getElementById("main"));
+ReactDOM.render(<App/>, document.getElementById("main"));
