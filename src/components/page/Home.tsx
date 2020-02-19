255ab9ee3a04271c775af1b1d51e26cc99a4131e
keiya01 <keiya.s.0210@gmail.com>
2020-02-19 16:25:37 +0900

    Update Home page

--- src/components/page/Home.tsx
+++ src/components/page/Home.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 
@@ -7,13 +6,36 @@ const Container = styled.div`
   background-color: #292929;
   width: 100%;
   height: 100vh;
+  padding: 1rem 0;
+`;
+
+const TopContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 100px;
+  margin-bottom: 30px;
+`;
+
+const Title = styled.h1`
+  font-size: 5rem;
+  color: #fff;
+`;
+
+const Description = styled.p`
+  margin-top: 50px;
+  font-size: 2rem;
+  color: #aaa;
 `;
 
 const Home: React.FC = () => {
   return (
     <Container>
-      <h1>Top Page!</h1>
-      <Link to="/about">about</Link>
+      <TopContent>
+        <Title>High Performance Todo</Title>
+        <Description>This project for making high performance App in React.</Description>
+      </TopContent>
     </Container>
   )
 };
